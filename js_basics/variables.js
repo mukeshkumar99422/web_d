97f97143486ad894478aba1bfd30bfe4a8969aec
mukeshkumar99422 <156451966+mukeshkumar99422@users.noreply.github.com>
2024-04-13 18:00:31 +0000

    done

---
+++ js_basics/variables.js
@@ -0,0 +1,17 @@
+//declaring constants
+const accountId=144553
+//declaring variables(prefer to use let)
+let accountState;
+let accountEmail="hitesh@google.com"   //functional scope     
+var accountPassword="1234"             //block scope
+//we can declare variables like this(prefer not to use this)
+accountCity="jaipur"
+
+// accountId=2     //constant change not allowed
+console.log(accountId);
+
+accountEmail="mukeshkumargoogle.com"
+accountPassword="212121"
+accountCity="benglore"
+//print in tabular form(multiple variables at once)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
