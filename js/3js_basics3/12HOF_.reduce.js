a48eb2e48fc6614115e07678bded2952073ae5f0
mukeshkumar99422 <156451966+mukeshkumar99422@users.noreply.github.com>
2024-07-23 08:27:54 +0530

    DONE

--- js/3js_basics3/12HOF_.reduce.js
+++ js/3js_basics3/12HOF_.reduce.js
@@ -4,15 +4,15 @@
 //     ,initialvalue
 // )
 
-const myNums=[1,2,3];
+const myNums=[1,2,3,4];
 const myTotal1=myNums.reduce(function (acc,curr){  //initially 0 will be given to acc--next time sum will be given to acc
     console.log(`acc:${(acc)} and curr:${(curr)}`);
     return acc+curr;
 },0);
-console.log(myTotal1);
+console.log(myTotal1);                                   //1+2+3+4
 
 //using arrow function
-const myTotal2=myNums.reduce((acc,curr)=>(acc*curr),1);
+const myTotal2=myNums.reduce((acc,curr)=>(acc*curr),1);  //1*2*3*4
 console.log(myTotal2);
 
 //reduce=>array of object
